d4fb9e58bcb8e12fe28855506e52ba70acf38014
Julius Angelo Paano <jul.paano@leekie.com>
2024-10-21 16:16:39 +0800

    View Icon

--- resources/frontend/components/Icon.js
+++ resources/frontend/components/Icon.js
@@ -14,6 +14,7 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import CloseIcon from "@mui/icons-material/Close";
 import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
 import SwapVertIcon from "@mui/icons-material/SwapVert";
+import VisibilityIcon from "@mui/icons-material/Visibility";
 
 const ICON_TYPES = {
     dashboard: DashboardIcon,
@@ -31,6 +32,7 @@ const ICON_TYPES = {
     close: CloseIcon,
     addImage: AddPhotoAlternateIcon,
     sort: SwapVertIcon,
+    view: VisibilityIcon,
 };
 
 export default function Icon({ icon, size, onClick, className }) {
